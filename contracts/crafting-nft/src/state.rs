be1a9b7a6aace462d9eaa4d352b684701054c1e4
haitranwang <haitranwang@gmail.com>
2024-03-07 09:56:06 +0700

    Revert "remove redundant spaces"
    This reverts commit a6297a1ee3a982afddc94dc96f15d4c6b4ec5310.

--- contracts/crafting-nft/src/state.rs
+++ contracts/crafting-nft/src/state.rs
@@ -78,23 +78,85 @@ pub struct Metadata {
 }
 
 pub const WHITE_GEM_WORK_POWER: Item<[Decimal; 7]> = Item::new("white gem work power");
+// [Decimal; 7] =[
+//     Decimal::from_str("2").unwrap(),
+//     Decimal::from_str("3").unwrap(),
+//     Decimal::from_str("5").unwrap(),
+//     Decimal::from_str("8").unwrap(),
+//     Decimal::from_str("13").unwrap(),
+//     Decimal::from_str("21").unwrap(),
+//     Decimal::from_str("34").unwrap(),
+// ];
+
 pub const BLUE_GEM_WORK_POWER: Item<[Decimal; 7]> = Item::new("blue gem work power");
+// [Decimal; 7] =[
+//     Decimal::from_str("22.5").unwrap(),
+//     Decimal::from_str("33.75").unwrap(),
+//     Decimal::from_str("56.25").unwrap(),
+//     Decimal::from_str("90").unwrap(),
+//     Decimal::from_str("146.25").unwrap(),
+//     Decimal::from_str("236.25").unwrap(),
+//     Decimal::from_str("382.5").unwrap(),
+// ];
+
 pub const GOLD_GEM_WORK_POWER: Item<[Decimal; 7]> = Item::new("gold gem work power");
+// [Decimal; 7] =[
+//     Decimal::from_str("120").unwrap(),
+//     Decimal::from_str("180").unwrap(),
+//     Decimal::from_str("300").unwrap(),
+//     Decimal::from_str("480").unwrap(),
+//     Decimal::from_str("780").unwrap(),
+//     Decimal::from_str("1260").unwrap(),
+//     Decimal::from_str("2040").unwrap(),
+// ];
+
 pub const RED_GEM_WORK_POWER: Item<[Decimal; 7]> = Item::new("red gem work power");
+// [Decimal; 7] =[
+//     Decimal::from_str("360").unwrap(),
+//     Decimal::from_str("540").unwrap(),
+//     Decimal::from_str("900").unwrap(),
+//     Decimal::from_str("1440").unwrap(),
+//     Decimal::from_str("2340").unwrap(),
+//     Decimal::from_str("3780").unwrap(),
+//     Decimal::from_str("6120").unwrap(),
+// ];
+
 pub const GEM_RATIO: Item<[Decimal; 4]> = Item::new("gem ratio");
+// [Decimal; 4] = [
+//     Decimal::from_str("0.9").unwrap(),
+//     Decimal::from_str("0.08").unwrap(),
+//     Decimal::from_str("0.015").unwrap(),
+//     Decimal::from_str("0.005").unwrap(),
+// ];
+
 pub const GEM_WORK_LOAD: Item<[Decimal; 6]> = Item::new("gem work load");
+// [Decimal; 6] = [
+//     Decimal::from_str("3").unwrap(),
+//     Decimal::from_str("5").unwrap(),
+//     Decimal::from_str("8").unwrap(),
+//     Decimal::from_str("13").unwrap(),
+//     Decimal::from_str("21").unwrap(),
+//     Decimal::from_str("34").unwrap(),
+// ];
 
 pub const CONFIG: Item<Config> = Item::new("config");
+
 pub const RANDOM_SEED: Item<[u8; 32]> = Item::new("random seed");
+
 pub const RANDOM_JOBS: Map<String, RandomJob> = Map::new("random jobs");
+
 pub const AURAGON_LATEST_TOKEN_ID: Item<u64> = Item::new("auragon latest token id");
+
 pub const SHIELD_LATEST_TOKEN_ID: Item<u64> = Item::new("shield latest token id");
 
 // DeQueue to store the gem forging requests from users
 pub const USERS_IN_QUEUE: Deque<UserInfo> = Deque::new("users_in_queue");
+
 // Current Queue ID
 pub const CURRENT_QUEUE_ID: Item<u64> = Item::new("current queue id");
+
 // Auragon URI
 pub const AURAGON_URI: Item<AuragonURI> = Item::new("auragon uri");
+
 // Shield URI
 pub const SHIELD_URI: Item<String> = Item::new("shield uri");
